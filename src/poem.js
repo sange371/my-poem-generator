15e5478c28f1ca1e3dc2cbc7e9d46150177f4887
sange371 <zusangembaleki@icloud.com>
2025-06-19 22:15:26 +0200

    changed the whole code

--- src/poem.js
+++ src/poem.js
@@ -1,14 +1,24 @@
-function generatePoem(event){
-    event.preventDefault();
-
-   
+function poemResponse(response){
+    console.log(response.data.answer);
 
     new Typewriter("#poem-output",{
-        strings:"I love flowers",
+        strings:response.data.answer,
         autoStart:true,
         cursor:" ",
-        delay:20,
+        delay:40,
     });
+
 }
-let poemButton=document.querySelector("#poem-button");
-poemButton.addEventListener("click",generatePoem);
+function poemGenerator(event){
+    event.preventDefault();
+
+    let poemTopic=document.querySelector("#poem-input");
+    let context="Please generate a 4 line poem only.Please follow prompt from the user.";
+    let prompt=`Please write me a poem of 4 lines using basic HTML ,the poem must be about ${poemTopic.value}`;
+    let apiKey="abod483e2dfa17bb1244b3fbdft2032b";
+    let apiUrl=`https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+
+    axios.get(apiUrl).then(poemResponse);
+}
+let button=document.querySelector("#poem-button");
+button.addEventListener("click",poemGenerator);
